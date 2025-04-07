31d2f05ff2f7d2078b7d47014b83099b43af22bd
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2025-04-07 20:56:42 +0530

    Task-02 - Simple OOP

---
+++ Task-02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Person {
+  string name;
+  int age;
+  public Person(string name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public void Introduce() {
+    Console.WriteLine($"My name is {name} and I'm {age} years old.");
+  }
+}
+public class Task02 {
+  public static void Main(string[] args)
+  {
+    Person p1 = new Person("Vasu", 22);
+    p1.Introduce();
+    Person p2 = new Person("Mathi", 19);
+    p2.Introduce();
+  }
+}
+  
