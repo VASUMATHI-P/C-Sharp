ec46ba3a76155f5a2defba0b9a902dc3bd242c45
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2025-04-07 20:55:44 +0530

    Task-01 - Basic Data Types, Control Structures, and Methods

---
+++ Task-01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive integer:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num < 0) {
+                Console.WriteLine("Please Enter a positive integer.");
+            } else {
+                Console.WriteLine("Factorial of " + num + " is " + Factorial(num));
+            }
+        }
+
+        static int Factorial(int num) {
+            int fact = 1;
+            for(int i=1; i<=num; i++){
+                fact *= i;
+            }
+            return fact;
+        }
+    }
+}
