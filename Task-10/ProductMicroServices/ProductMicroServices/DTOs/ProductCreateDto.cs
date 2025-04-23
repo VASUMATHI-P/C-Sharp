94f300d0b767028b7199b93e50b83eec3bd994b0
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2025-04-23 19:01:20 +0530

    Refactor: Add ProductCreateDto for POST method and update product model

--- Task-10/ProductMicroServices/ProductMicroServices/DTOs/ProductCreateDto.cs
+++ Task-10/ProductMicroServices/ProductMicroServices/DTOs/ProductCreateDto.cs
@@ -1,11 +1,21 @@
-﻿namespace ProductMicroServices.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductMicroServices.DTOs
 {
     public class ProductCreateDto
     {
+        [Required(ErrorMessage = "Product name is required")]
         public string Name { get; set; }
+
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Product Category is required")]
         public string Category { get; set; }
+
+        [Range(0.01, 100000, ErrorMessage = "Price must be between 0.01 and 100000")]
         public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock cannot be a Negative value")]
         public int Stock { get; set; }
     }
 }
